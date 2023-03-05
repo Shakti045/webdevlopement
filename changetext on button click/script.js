af15a6bf77a46bb41c4926740cd97a2d1d1a0b6a
Shakti045 <122844750+Shakti045@users.noreply.github.com>
2023-03-05 09:06:38 +0530

    Create script.js

---
+++ changetext on button click/script.js
@@ -0,0 +1,11 @@
+
+function textchanger(){
+let v=document.getElementById("display")
+if(v.innerText=="The most affordable learning platform"){
+  v.innerText="PW SKILLS"
+}else{
+  v.innerText="The most affordable learning platform"
+}
+
+
+}
